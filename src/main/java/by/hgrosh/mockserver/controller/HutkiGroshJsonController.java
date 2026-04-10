ec113aabb4c9708f5bfd0a765be70490ce056f5d
@dzmitryi.miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2026-04-10 17:36:06 +0300

    Step 22: Maximum detailed JSON logging for diagnostics

--- src/main/java/by/hgrosh/mockserver/controller/HutkiGroshJsonController.java
+++ src/main/java/by/hgrosh/mockserver/controller/HutkiGroshJsonController.java
@@ -1,6 +1,8 @@
 package by.hgrosh.mockserver.controller;
 
 import by.hgrosh.mockserver.model.DataStore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +16,20 @@ import java.util.List;
 public class HutkiGroshJsonController {
 
     private static final Logger log = LoggerFactory.getLogger(HutkiGroshJsonController.class);
+    private static final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+    private void logJson(String title, Object obj) {
+        try {
+            String json = mapper.writeValueAsString(obj);
+            System.out.println(">>> [JSON-DEBUG] " + title + ":\n" + json);
+        } catch (Exception e) {
+            log.error("Failed to log JSON: {}", e.getMessage());
+        }
+    }
 
     @PostMapping("/info")
     public AccountInfoResponse accountInfo(@RequestBody AccountInfoRequest req) {
-        log.info(">>> [JSON] accountInfo request for: {}", req.account);
+        logJson("INCOMING AccountInfoRequest", req);
         
         AccountInfoResponse res = new AccountInfoResponse();
         DataStore.Invoice inv = DataStore.invoiceStore.get(req.account);
@@ -37,29 +49,32 @@ public class HutkiGroshJsonController {
             res.ticket.add("Ошибка: Лицевой счет не найден в базе ПУ");
         }
         
+        logJson("OUTGOING AccountInfoResponse", res);
         return res;
     }
 
     @PostMapping("/submit")
     public SubmitPaymentResponse submitPayment(@RequestBody SubmitPaymentRequest req) {
-        log.info(">>> [JSON] submitPayment request for: {}, amount: {}", req.account, req.amount);
+        logJson("INCOMING SubmitPaymentRequest", req);
         
         SubmitPaymentResponse res = new SubmitPaymentResponse();
-        res.responseCode = "allow"; // ОБЯЗАТЕЛЬНО для ТХГ
+        res.responseCode = "allow";
         res.unipayTrxId = System.currentTimeMillis() / 1000;
         res.ticket.add("Оплата инициирована");
         
+        logJson("OUTGOING SubmitPaymentResponse", res);
         return res;
     }
 
     @PostMapping("/commit")
     public ConfirmPaymentResponse confirmPayment(@RequestBody ConfirmPaymentRequest req) {
-        log.info(">>> [JSON] confirmPayment request for trx: {}, confirmed: {}", req.unipayTrxId, req.confirmed);
+        logJson("INCOMING ConfirmPaymentRequest", req);
         
         ConfirmPaymentResponse res = new ConfirmPaymentResponse();
-        res.responseCode = "allow"; // ОБЯЗАТЕЛЬНО для ТХГ
+        res.responseCode = "allow";
         res.ticket.add("Оплата подтверждена");
         
+        logJson("OUTGOING ConfirmPaymentResponse", res);
         return res;
     }
 
