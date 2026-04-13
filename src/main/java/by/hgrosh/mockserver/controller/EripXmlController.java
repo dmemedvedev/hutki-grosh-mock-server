fe0b34107366970dded0bd9efbfd5dd251a07f37
@dzmitryi.miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2026-04-13 15:28:29 +0300

    Step 41.6: Fix accountInfo call (add missing 3rd argument)

--- src/main/java/by/hgrosh/mockserver/controller/EripXmlController.java
+++ src/main/java/by/hgrosh/mockserver/controller/EripXmlController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.util.*;
 
@@ -90,7 +89,7 @@ public class EripXmlController {
                     }
                 }
 
-                HutkiGroshJsonController.AccountInfoResponse jsonRes = jsonController.accountInfo(jsonReq, null);
+                HutkiGroshJsonController.AccountInfoResponse jsonRes = jsonController.accountInfo(jsonReq, null, account);
                 outXml = buildServiceInfoResponse(jsonRes, requestId);
 
             } else if ("TransactionStart".equals(type)) {
